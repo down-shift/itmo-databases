8c74d8f2b3c176fd3b83d7a66eac6ce8cd3c4f90
down-shift <anicegoose@gmail.com>
2024-02-23 13:15:54 +0300

    add

--- lab1/create.sql
+++ lab1/create.sql
@@ -60,3 +60,42 @@ CREATE TABLE IF NOT EXISTS VehicleUsage (
 
 -- Insert values
 
+INSERT INTO Person (name, house_id, age, gender) VALUES
+    ('Dr Floyd', 1, 69, 0.5),
+    ('HAPIHAPIHAPICAT', 2, 2, 2.2),
+    ('Alexey', 3, 46, 0.0),
+    ('Maxwell', 4, 3, -2.1);
+
+INSERT INTO Item (name, function, owner_id, is_part_of_heritage) VALUES
+    ('flashlight', 'enlighten the surroundings', 1, 1),
+    ('keychain', 'carefully store finely picked keys', 2, 0),
+    ('Sergey', 'no specific purpose', 4, 0),
+    ('heart', 'be brave', 3, 1);
+
+INSERT INTO Location (name, description, coords) VALUES
+    ('Florida plane', 'Florida plane', POINT(44.3340, 38.0437)),
+    ('saint petersburg', 'pain pain pain', POINT(59.8, 30.3)),
+    ('gelendzhik', 'mostly paradise', POINT(180.0, 180.0));
+
+INSERT INTO Building (name, occupant, location_id) VALUES
+    ('itmo', 2, 2),
+    ('dvorets', 4, 1),
+    ('panelka1', 3, 3),
+    ('panelka2', 1, 2);
+
+INSERT INTO Infrastructure (name, location_id, height) VALUES
+    ('crane', 1, 25),
+    ('projector', 1, 5),
+    ('dock', 2, 2);
+
+INSERT INTO Vehicle (name, speed, owner) VALUES
+    ('plane', 456.7, 'pilot'),
+    ('spaceship', 356785678.2, 'aliens'),
+    ('car', 111.0, 'Dr Floyd');
+
+INSERT INTO VehicleUsage (person_id, vehicle_id, timestamp) VALUES
+    (1, 2, 04:05:06),
+    (1, 1, 08:05:00),
+    (2, 3, 23:45:11),
+    (3, 3, 16:02:24),
+    (4, 1, 24:02:22);
